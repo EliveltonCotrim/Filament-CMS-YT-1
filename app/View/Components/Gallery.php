d2891dbd465f08fd4cbb02d02dbe8d4205ff2b1f
Elivelton Cotrim <elivelton.gbi@gmail.com>
2025-05-01 12:00:47 -0300

    Corrige a formatação do comentário no método render da classe Gallery

--- app/View/Components/Gallery.php
+++ app/View/Components/Gallery.php
@@ -18,7 +18,7 @@ class Gallery extends Component
 
     /**
      * Get the view / contents that represent the component.
-     */
+        */
     public function render(): View|Closure|string
     {
         return view('components.gallery');
